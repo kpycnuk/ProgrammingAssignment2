0183c25741675d919e38470e7dc691094b9e3d37
KpycNuK <kpycnuk@gmail.com>
2015-06-21 22:09:16 +0300

    Revrite forked file by mine solution.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## For Programming Assignment 2
+## I have rewrite makeVector and cachemean function for matrix-needs. 
+## It looks as most reasonable variant ^^
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates a special "matrix" object that can cache its inverse.
+## This function contain a list of a function:
+    # 1) set the value of the matrix
+    # 2) get the value of the matrix
+    # 3) set the value of the inversed matrix
+    # 4) get the value of the inversed matrix
+makeCacheMatrix <- function(in_matrix = matrix()) {
+    inversed <- NULL
+    set <- function(set_matrix){
+        in_matrix <<- set_matrix
+        inversed <<- NULL
+    }
+    get <- function() in_matrix
+    setinverse <- function(inv) inversed <<- inv
+    getinverse <- function() inversed
+    
+    list(set = set,
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+# This function computes the inverse of the special "matrix" 
+# returned by makeCacheMatrix. 
+# If the inverse has already been calculated (and the matrix has not changed), 
+# then cacheSolve should retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## !!! This function assumes that the matrix is always invertible.
+cacheSolve <- function(cache_matrix, ...) {
+     inversed <- cache_matrix$getinverse()
+     if(!is.null(inversed)){
+         message("getting cached data")
+         ## Return a matrix that is the inverse of 'cache_matrix'
+         return(inversed)
+     }
+     data <- cache_matrix$get()
+     inversed <- solve(data, ...)
+     cache_matrix$setinverse(inversed)
+     # return inversed matrix
+     inversed
 }
